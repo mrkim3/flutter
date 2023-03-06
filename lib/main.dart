3f5bef93549fe471512f01b7e2c31b23f57ffd36
mrkim3 <kim.miri333@gmail.com>
2023-03-06 22:30:18 +0900

    수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:toonfix/widgets/Button.dart';
 
 class Player {
-  String name ;
+  String name;
 
   Player({required this.name});
-
 }
 
 void main() {
-   var miri = Player(name: "miri");
-  runApp(App()); 
-
+  var miri = Player(name: "miri");
+  runApp(const App());
 }
 
- class App extends StatelessWidget{
+class App extends StatelessWidget {
+  const App({super.key});
 
-
-    @override
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color(0xFF181818),
+        backgroundColor: const Color(0xFF181818),
         body: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: 40,
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
           ),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(
-                height: 20,
+              const SizedBox(
+                height: 80,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   Column(
                     children: [
-                      Text("Hey, Selena",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 28,
-                        fontWeight: FontWeight.w800,
-                      ),),
-                      Text("Welcome back",
-                      style: TextStyle(
-                        color: Colors.white.
-                        withOpacity(0.8),
-                        fontSize: 18,
-                      ),),
+                      const Text(
+                        "Hey, Selena",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Text(
+                        "Welcome back",
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.8),
+                          fontSize: 18,
+                        ),
+                      ),
                     ],
                   )
+                ],
+              ),
+              const SizedBox(
+                height: 120,
+              ),
+              Text(
+                'Total balance',
+                style: TextStyle(
+                    fontSize: 22, color: Colors.white.withOpacity(0.8)),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                '\$5 194 382',
+                style: TextStyle(
+                    fontSize: 48,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white.withOpacity(0.8)),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Button(
+                    text: 'Transfer',
+                    bgColor: Color(0xFFF1B33B),
+                    textColor: Colors.black,
+                  ),
+                  Button(
+                    text: 'Request',
+                    bgColor: Color(0xFF1F2123),
+                    textColor: Colors.white,
+                  ),
                 ],
               ),
             ],
